030896c723bd223c61474d666482aeef7c465001
Jacob Anderson <jacobdanderson@gmail.com>
2024-08-23 23:36:08 -0600

    Adjusting Colors

--- front-end/src/pages/about.vue
+++ front-end/src/pages/about.vue
@@ -8,19 +8,12 @@ const about = computed(() => store.characters.description); // Accessing 'about'
 
 <template>
 	<div class="page">
-		<img alt="RetroZetro Comics Logo" class="logo" src="https://retrozetrocomics.s3.amazonaws.com/images/Original_Icon.png"> <!-- v-if="$route.path !== '/'" -->
-		<!--		<img -->
-		<!--			v-if="$route.path === '/'" alt="Zetro's Profiles" class="profile-pic" -->
-		<!--			src="../assets/images/Zetro2.jpg" -->
-		<!--		> -->
+		<img alt="RetroZetro Comics Logo" class="logo" src="https://retrozetrocomics.s3.amazonaws.com/images/Original_Icon.png">
 		<div class="item">
 			<h1>About RetroZetro Comics</h1>
 			<p>{{ about }}</p>
 		</div>
 	</div>
-	
-	<!-- Do not show Education Component on home page -->
-<!--	<EducationComponent v-if="$route.path !== '/'" /> -->
 </template>
 
 <style scoped>
@@ -29,15 +22,6 @@ const about = computed(() => store.characters.description); // Accessing 'about'
 	width: 150px; /* Adjust size as needed */
 	height: auto;
 	margin: 0 auto 20px;
-}
-
-.profile-pic {
-	display: block;
-	border-radius: 5%;
-	width: 150px;
-	height: 100%;
-	object-fit: cover;
-	margin: 20px auto;
 }
 </style>
 
