78f8f79b0ae4b19366ca97e5871a5613aa0ad55d
Jacob Anderson <jacobdanderson@gmail.com>
2025-03-14 22:06:58 -0600

    eslint configuration

--- front-end/src/typed-router.d.ts
+++ front-end/src/typed-router.d.ts
@@ -23,7 +23,6 @@ declare module 'vue-router/auto-routes' {
     '/about': RouteRecordInfo<'/about', '/about', Record<never, never>, Record<never, never>>,
     '/characters': RouteRecordInfo<'/characters', '/characters', Record<never, never>, Record<never, never>>,
     '/contact': RouteRecordInfo<'/contact', '/contact', Record<never, never>, Record<never, never>>,
-    '/hi/[name]': RouteRecordInfo<'/hi/[name]', '/hi/:name', { name: ParamValue<true> }, { name: ParamValue<false> }>,
     '/README': RouteRecordInfo<'/README', '/README', Record<never, never>, Record<never, never>>,
   }
 }
