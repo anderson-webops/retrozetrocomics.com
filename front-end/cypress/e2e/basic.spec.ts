eafe606f2687fceef3678e16721bdafdfee388e4
Jacob Anderson <jacobdanderson@gmail.com>
2026-08-22 16:39:27 -0400

    Align browser checks with Tyler content

--- front-end/cypress/e2e/basic.spec.ts
+++ front-end/cypress/e2e/basic.spec.ts
@@ -6,12 +6,13 @@ context("Basic", () => {
 	it("shows the home page and navigates to Characters", () => {
 		cy.url().should("eq", `${Cypress.config().baseUrl}/`);
 		cy.contains("RetroZetro Comics").should("exist");
-		cy.contains("Two storylines drive the Retroverse").should("exist");
+		cy.contains("Story files, characters, and worlds").should("exist");
 
 		cy.contains("Characters").click();
 		cy.url().should("eq", `${Cypress.config().baseUrl}/characters`);
-		cy.contains("Meet the Characters").should("exist");
-		cy.contains("Zetro").should("exist");
+		cy.contains("Meet Tyler's Characters").should("exist");
+		cy.contains("Exo Dexus").should("exist");
+		cy.contains("Fazo").should("exist");
 	});
 
 	it("navigates to About from the header", () => {
